977e934fb0c4a2e5142ac47448295b0b1d3349a8
Vincenzo Tramo <vv.tramo@gmail.com>
2024-11-10 21:40:20 +0100

    feat: add refresh token request

---
+++ src/security/auth/oauth/refresh_token_request.rs
@@ -0,0 +1,57 @@
+use reqwest::Url;
+use crate::security::auth::oauth::OAuthAuthorizationResponse;
+
+pub struct OAuthRefreshTokenRequest {
+    refresh_token_endpoint: Url,
+    client_id: String,
+    client_secret: String,
+    scope: Vec<String>,
+    refresh_token: String,
+}
+
+impl OAuthRefreshTokenRequest {
+    const GRANT_TYPE: &'static str = "grant_type";
+    const REFRESH_TOKEN: &'static str = "refresh_token";
+    const CLIENT_ID: &'static str = "client_id";
+    const CLIENT_SECRET: &'static str = "client_secret";
+    const SCOPE: &'static str = "scope";
+
+    pub fn new(
+        refresh_token_endpoint: &Url,
+        client_id: &str,
+        client_secret: &str,
+        scope: &Vec<String>,
+        refresh_token: &str,
+    ) -> Self {
+        Self {
+            refresh_token_endpoint: refresh_token_endpoint.clone(),
+            client_id: client_id.to_string(),
+            client_secret: client_secret.to_string(),
+            scope: scope.clone(),
+            refresh_token: refresh_token.to_string(),
+        }
+    }
+
+    pub async fn send(&self) -> anyhow::Result<OAuthAuthorizationResponse> {
+        let client = reqwest::Client::new();
+        let authorization_response = client
+            .post(&self.refresh_token_endpoint.to_string())
+            .form(&self.to_params())
+            .send()
+            .await?
+            .json::<OAuthAuthorizationResponse>()
+            .await?;
+
+        Ok(authorization_response)
+    }
+
+    fn to_params(&self) -> Vec<(&'static str, String)> {
+        vec![
+            (Self::GRANT_TYPE, Self::REFRESH_TOKEN.to_string()),
+            (Self::REFRESH_TOKEN, self.refresh_token.clone()),
+            (Self::CLIENT_ID, self.client_id.clone()),
+            (Self::CLIENT_SECRET, self.client_secret.clone()),
+            (Self::SCOPE, self.scope.join(" ")),
+        ]
+    }
+}
